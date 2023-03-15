42a7487800030c0de120552408e73479a5d261fb
nurdan-sys <nurdan.alpbek@gmail.com>
2023-03-14 14:18:18 -1100

    api

---
+++ html/script.js
@@ -0,0 +1,23 @@
+
+
+// id = 1
+// fetch(`https://swapi.dev/api/people/ ${id}`)
+//     .then(res => res.json())
+//     .then((data) => alert(`Name: ${data.name}, Age: ${data.birth_year}`))
+
+
+// id = 12, 22
+// fetch(`https://swapi.dev/api/starships/ ${id}`)
+//     .then(res => res.json())
+//     .then((data) => alert(`Name: ${data.name}, ${data.model}`))
+
+
+// id = 30
+// fetch(`https://swapi.dev/api/vehicles/ ${id}`)
+//     .then(res => res.json())
+//     .then((data) => alert(`Name: ${data.name}, Model: ${data.model}`))
+
+// id = 14
+// fetch(`https://swapi.dev/api/vehicles/ ${id}`)
+//     .then(res => res.json())
+//     .then((data) => alert(`Name: ${data.name}, Model: ${data.model}`))
